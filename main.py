84685035e99209f1ae72a670f48ef2b8b2f79518
FeRsOmBrA <fcastano5@estudiantes.areandina.edu.co>
2024-02-07 00:15:08 -0500

    Finalizando Proyecto - Render

--- main.py
+++ main.py
@@ -4,25 +4,14 @@ import pandas as pd
 from src.utils import utils
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
-
+import pickle
 # Cargar los datos
 steam_games = pd.read_pickle("./data/steam_games.pkl")
 user_reviews = pd.read_pickle("./data/user_reviews.pkl")
 users_items = pd.read_pickle("./data/users_items.pkl")
-
-
-# Crear una matriz de utilidad a partir de la tabla de usuarios e items
-matrix_utility = users_items.pivot(
-    index='item_id', columns='user_id', values='playtime_forever').fillna(0)
-
-# Convertir la matriz de utilidad a una matriz sparse para mejorar la eficiencia
-matrix_utility_sparse = csr_matrix(matrix_utility.values)
-
-model_knn = NearestNeighbors(
-    metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
-
-# Entrenar el modelo
-model_knn.fit(matrix_utility_sparse)
+matrix_utility = pickle.load(open("./data/matrix_utility.pkl", "rb"))
+model_knn = pickle.load(open("./data/model_knn.pkl", "rb"))
+steam_games_model = pickle.load(open("./data/steam_games_model.pkl", "rb"))
 
 app = FastAPI()
 
@@ -53,5 +42,5 @@ async def get_developer_reviews_analysis(desarrolladora: str):
 
 @app.get("/recomendacion-juego/{item_id}")
 async def get_recomendacion_juego(item_id: str):
-    result = utils.recomendacion_juego(item_id, steam_games, matrix_utility, model_knn, k=5)
+    result = utils.recomendacion_juego(item_id, steam_games_model, matrix_utility, model_knn, k=5)
     return result
